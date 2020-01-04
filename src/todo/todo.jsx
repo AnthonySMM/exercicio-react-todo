bc3d3b60225b96ba74a914351e0007f093c96a2a
Anthony <nyazus@gmail.com>
2020-01-04 11:32:49 -0300

    finalizado

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -4,6 +4,7 @@ import axios from 'axios'
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
+import { throws } from 'assert'
 
 const URL = 'http://localhost:3003/api/todos'
 
@@ -13,29 +14,82 @@ export default class Todo extends Component{
 
        super(props)
        this.state = { description: ' ', list: [] } //estado inicial do objeto precisa do state
+      
        this.handleChange = this.handleChange.bind(this)
        this.handleAdd = this.handleAdd.bind(this)
+       this.handleSearch = this.handleSearch.bind(this)
+       this.handleClear = this.handleClear.bind(this)
+
+
+       this.handleRemove = this.handleRemove.bind(this)
+       this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
+       this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
+       
+       this.refresh()
    }
 
    handleChange(e){
        this.setState({...this.state , description  : e.target.value})
    }
 
+   //pegar a lista mais atualizada
+   //eh uma promisse
+   refresh(description = ''){
+       const search = description ? `&description__regex=/${description}/` : ''
+       axios.get(`${URL}?sort=-createdAT${search}`)
+       .then(resp=> this.setState({...this.state, description, list: resp.data}))
+   }
+
+
+   handleSearch(){
+       this.refresh(this.state.description)
+   }
     //funcao de add
     handleAdd(){
         const description = this.state.description
         axios.post(URL, { description})
-            .then(resp => console.log('Funcionou!'))
+            .then(resp => this.refresh())
     }
+
+    handleRemove(todo){
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp=> this.refresh(this.state.description))
+
+    }
+
+    handleMarkAsDone(todo){
+        axios.put(`${URL}/${todo._id}`,{...todo, done:true})
+            .then(resp => this.refresh(this.state.description))
+    }
+
+    handleMarkAsPending(todo){
+        axios.put(`${URL}/${todo._id}`,{...todo, done:false})
+            .then(resp=> this.refresh(this.state.description))
+    }
+
+    handleClear(){
+        this.refresh()
+    }
+
+    
     
     render(){
         return (
             <div>
                 <PageHeader name = 'Tarefas'small = ' Cadastro'></PageHeader> 
-             <TodoForm description = {this.state.description}
+             <TodoForm 
+             description = {this.state.description}
              handleChange={this.handleChange}
-             handleAdd={this.handleAdd} />
-             <TodoList/>
+             handleAdd={this.handleAdd} 
+             handleSearch = {this.handleSearch}
+             handleClear = {this.handleClear}/>
+
+
+             <TodoList list={this.state.list}
+             handleMarkAsDone={this.handleMarkAsDone}
+             handleMarkAsPending={this.handleMarkAsPending}
+             handleRemove={this.handleRemove} />
+             
             </div>
         )
     }
--- src/todo/todoList.jsx
+++ src/todo/todoList.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
+import IconButton from '../template/iconButton'
 
-export default props =>(
-    <div>
-        <h1>Lista</h1>
-    </div>
-)
+export default props =>{
+
+    const renderRows =()=>{
+        const list = props.list || []
+        return list.map(todo => (
+        <tr key={todo._id} /* chave utilizada é a mesma do mongo */>
+                <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
+                <td>
+                    <IconButton style='success' icon ='check' hide={todo.done}
+                     onClick={()=> props.handleMarkAsDone(todo) /*botão para marcar o que foi feito*/}></ IconButton>
+
+                     <IconButton style='warning' icon='undo' hide={!todo.done}
+                     onClick={()=> props.handleMarkAsPending(todo) /*botão para marcar o que vou fazer*/}></IconButton>
+
+                    <IconButton style = 'danger' icon='trash-o' hide={!todo.done}
+                     onClick={() =>props.handleRemove(todo)}></IconButton>
+                </td>
+                
+            </tr>
+        ))
+    }
+
+    return(
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th>Descrição</th>
+                    <th className='tableActions'>Ações</th>
+                </tr>
+            </thead>
+                <tbody>
+                {renderRows()}
+                </tbody>
+        </table>
+    )
+}
